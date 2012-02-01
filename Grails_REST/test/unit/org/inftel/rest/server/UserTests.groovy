5e3888aa1899321a6444ffde1a9a977f1d198f2e
Alberto Jimenez <ajr_neo@hotmail.com>
2012-02-01 15:24:47 +0100

    Creado Test Unitario para la clase User

--- Grails_REST/test/unit/org/inftel/rest/server/UserTests.groovy
+++ Grails_REST/test/unit/org/inftel/rest/server/UserTests.groovy
@@ -13,7 +13,24 @@ import org.junit.*
 @TestFor(User)
 class UserTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+	void testConstraints() {
+		def will = new User(userId: "william")
+		mockForConstraintsTests(User, [will])
+
+		// La validacion debe fallar porque no se ha introducido ni usuario ni pass
+		def testUser = new User() 
+		assert !testUser.validate() 
+		assertEquals "nullable", testUser.errors["userId"] 
+		assertEquals "nullable", testUser.errors["password"]
+		
+		// La validacion debe fallar porque el usuario y la pass son iguales
+		testUser = new User(userId: "william", password: "william") 
+		assert !testUser.validate()
+		assertEquals "unique", testUser.errors["userId"] 
+		assertEquals "validator", testUser.errors["password"]
+		
+		// En esta ocasion la validacion de usuario tiene que pasar
+		testUser = new User(userId: "glen", password: "passwd") 
+		assert testUser.validate()
+	}
 }
